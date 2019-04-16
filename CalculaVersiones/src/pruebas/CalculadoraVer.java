df7b1dfb789399670f670a232c0e7df7383d818a
Adrian Arpi <arpiadrian5@gmail.com>
2019-04-16 09:31:29 +0200

    Se ha agregado un constructor con dos parametros

---
+++ CalculaVersiones/src/pruebas/CalculadoraVer.java
@@ -0,0 +1,18 @@
+package pruebas;
+
+public class CalculadoraVer {
+
+	//Constructo con dos parametros
+	public CalculadoraVer(int num1, int num2){
+		
+		
+		
+		
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
