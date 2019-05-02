4b6d2b4f2f27a7e5ae1c91b54eb65b45db9173db
Adrian Arpi <arpiadrian5@gmail.com>
2019-05-02 18:43:39 +0200

    Personalización con tamaños de fuente.

--- PildorasInformaticas/src/Eventos/SintaxisRadio.java
+++ PildorasInformaticas/src/Eventos/SintaxisRadio.java
@@ -1,7 +1,13 @@
 package Eventos;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 
@@ -9,63 +15,107 @@ public class SintaxisRadio {
 
 	public static void main(String[] args) {
 
-		MarcoRadio miMarco=new MarcoRadio();
+		MarcoRadio miMarco = new MarcoRadio();
 		miMarco.setVisible(true);
 		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 	}
 
 }
 
-class MarcoRadio extends JFrame{
-	
+class MarcoRadio extends JFrame {
+
 	public MarcoRadio() {
-		
+
 		setTitle("Radio Button");
-		setBounds(550,300,500,300);
+		setBounds(550, 300, 500, 300);
 		setLocationRelativeTo(null);
-		
-		LaminaRadio miLamina=new LaminaRadio();
+
+		LaminaRadio miLamina = new LaminaRadio();
 		add(miLamina);
 	}
 }
 
-class LaminaRadio extends JPanel{
-	
+class LaminaRadio extends JPanel {
+
 	JRadioButton btn1;
 	JRadioButton btn2;
 	JRadioButton btn3;
 	JRadioButton btn4;
 	JRadioButton btn5;
 	
+	JLabel miTexto;
+
 	public LaminaRadio() {
+
+		setLayout(new BorderLayout());
+
+		miTexto = new JLabel("Esto es una prueba");
+		add(miTexto,BorderLayout.CENTER);
 		
-		//Primero determinamos un grupo de botones y luego los creamos, el grupo sirve para que solo uno se seleccione
-		ButtonGroup miGrupo1=new ButtonGroup();
-		btn1=new JRadioButton("Azul",false); //Estado inicial sin seleccioner
-		btn2=new JRadioButton("Rojo",true); //Estado inicial  seleccionado
-		btn3=new JRadioButton("Verde",false); //Estado inicial sin seleccioner
-		
-		//Los añadimos al grupo
+
+		JPanel lamina2 = new JPanel();
+		add(lamina2, BorderLayout.SOUTH);
+
+		// Primero determinamos un grupo de botones y luego los creamos, el grupo sirve
+		// para que solo uno se seleccione
+		ButtonGroup miGrupo1 = new ButtonGroup();
+		btn1 = new JRadioButton("pequeño", false); // Estado inicial sin seleccioner
+		btn2 = new JRadioButton("Mediano", true); // Estado inicial seleccionado
+		btn3 = new JRadioButton("Grande", false); // Estado inicial sin seleccioner
+
+		// Le damos a boton1 la escucha
+		btn1.addActionListener(new ManejoRadios());
+		btn2.addActionListener(new ManejoRadios());
+		btn3.addActionListener(new ManejoRadios());
+//		btn4.addActionListener(new ManejoRadios()); --> Esto nos genera un error xq aun no esta creado
+
+		// Los añadimos al grupo
 		miGrupo1.add(btn1);
 		miGrupo1.add(btn2);
 		miGrupo1.add(btn3);
-		
-		//No se puede agregar el grupo, debemos agregar uno a uno los botones
-		add(btn1);
-		add(btn2);
-		add(btn3);
-		
-		 //Nuevo grupo
-		ButtonGroup miGrupo2=new ButtonGroup();
-		btn4=new JRadioButton("Masculino",false);
-		btn5=new JRadioButton("Femenino",false);
-		
+
+		// No se puede agregar el grupo, debemos agregar uno a uno los botones
+		lamina2.add(btn1, BorderLayout.SOUTH);
+		lamina2.add(btn2, BorderLayout.SOUTH);
+		lamina2.add(btn3, BorderLayout.SOUTH);
+
+		// Nuevo grupo
+		ButtonGroup miGrupo2 = new ButtonGroup();
+		btn4 = new JRadioButton("Muy grande", false);
+		btn5 = new JRadioButton("No hago nada xd", false);
+
 		miGrupo2.add(btn4);
 		miGrupo2.add(btn5);
+
+		lamina2.add(btn4, BorderLayout.SOUTH);
+		lamina2.add(btn5, BorderLayout.SOUTH);
 		
-		add(btn4);
-		add(btn5);
-		
+		btn4.addActionListener(new ManejoRadios());
+		btn5.addActionListener(new ManejoRadios());
+	}
+
+	private class ManejoRadios implements ActionListener {
+
+		@Override
+		public void actionPerformed(ActionEvent e) {
+
+			if (btn1.isSelected()) {
+
+				miTexto.setFont(new Font("Serif", Font.PLAIN, 10));
+
+			}else if(btn2.isSelected()){
+				miTexto.setFont(new Font("Serif", Font.PLAIN, 12));
+				
+			}else if(btn3.isSelected()){
+				miTexto.setFont(new Font("Serif", Font.PLAIN, 18));
+				
+			}else if(btn4.isSelected()){
+				miTexto.setFont(new Font("Serif", Font.PLAIN, 30));
+				
+			}
+
+		}
+
 	}
 }
