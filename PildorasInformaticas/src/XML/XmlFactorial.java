e25fbb938b8ec50ae04d6d6b0b7e119f0cfef02e
Adrian Arpi <arpiadrian5@gmail.com>
2019-05-22 11:32:10 +0200

    Creación desde cero de archivos XML

--- PildorasInformaticas/src/XML/XmlFactorial.java
+++ PildorasInformaticas/src/XML/XmlFactorial.java
@@ -1,9 +1,17 @@
 package XML;
 
+import java.awt.BorderLayout;
+import java.awt.Font;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.xml.stream.XMLOutputFactory;
 
 import org.jdom2.Document;
@@ -11,7 +19,18 @@ import org.jdom2.Element;
 import org.jdom2.output.Format;
 import org.jdom2.output.XMLOutputter;
 
-public class XmlFactorial {
+public class XmlFactorial extends JFrame{
+	
+	public XmlFactorial(){
+		
+		setTitle("COMPRA DE BILLETES");
+		
+		setSize(500, 500);
+		
+	
+		lamina miLamina=new lamina();
+		add(miLamina);
+	}
 
 	private static long factorial(int n){
 		
@@ -26,55 +45,94 @@ public class XmlFactorial {
 		return resul;
 	}
 	
+private class lamina extends JPanel{
 	
+	private JPanel lamina2;
+	private JTextArea area1;
+	private JLabel info;
+	private JScrollPane laminaBarras;
+	private JButton btnPrueba;
+	
+	public lamina(){
+		
+		setLayout(new BorderLayout());
+		
+		lamina2=new JPanel();
+		add(lamina2);
+		
+		Font negrita= new Font("Serif",Font.PLAIN,25);
+		info =new JLabel("Haz doble clic en el destino deseado");
+		info.setFont(negrita);
+		
+		lamina2.add(info,BorderLayout.NORTH);
+		
+		area1=new JTextArea(8,20);
+		lamina2.add(area1,BorderLayout.CENTER);
+		laminaBarras=new JScrollPane(area1);
+		lamina2.add(laminaBarras);
+		
+		btnPrueba=new JButton("Dale");
+		
+		laminaBarras.add(btnPrueba);
+	}
+	
+	
+}
 	
 	public static void main(String[] args) {
 		
-		int resp=0;
-		
-		try {
-			resp=Integer.parseInt(JOptionPane.showInputDialog(null,"Cuantos quieres"));
-			
-		} catch (NumberFormatException ex) {
-			
-			ex.printStackTrace();
-		}
-		
-		Element root=new Element("Factoriales");
-		Document doc=new Document(root);
-	
+		XmlFactorial miVentana=new XmlFactorial();
+		miVentana.setVisible(true);
+		miVentana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		miVentana.setLocationRelativeTo(null);
 		
 		
-		for (int i = 1; i <=resp; i++) {
-			
-			Element hijo=new Element("Factorial");
-			hijo.setAttribute("de", String.valueOf(i));
-			hijo.setText(String.valueOf(factorial(i)));
-			root.addContent(hijo);
-			
-			//Añadimos contenido al hijo
-			hijo.addContent("Prueba");
-			
-			
-			
-		}
-		
-		//Creamos un segundo elemento, lo añadimos al final y le agregamos un hijo
-		Element hijo2=new Element("Codigo");
-		root.addContent(hijo2);
-		Element sub2=new Element("Cod");
-		hijo2.addContent(sub2.addContent("54321"));
-		
-		//A sub2 le agregamos un atributo de tipo int
-		sub2.setAttribute("id", String.valueOf(50));
-		
-		
-		XMLOutputter serializador=new XMLOutputter(Format.getPrettyFormat());
-		try {
-			serializador.output(root, System.out); //doc --> NOs imprime todo el documento junto a la version (root,System.out)
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+//		
+//		int resp=0;
+//		
+//		try {
+//			resp=Integer.parseInt(JOptionPane.showInputDialog(null,"Cuantos quieres"));
+//			
+//		} catch (NumberFormatException ex) {
+//			
+//			ex.printStackTrace();
+//		}
+//		
+//		Element root=new Element("Factoriales");
+//		Document doc=new Document(root);
+//	
+//		
+//		
+//		for (int i = 1; i <=resp; i++) {
+//			
+//			Element hijo=new Element("Factorial");
+//			hijo.setAttribute("de", String.valueOf(i));
+//			hijo.setText(String.valueOf(factorial(i)));
+//			root.addContent(hijo);
+//			
+//			//Añadimos contenido al hijo
+//			hijo.addContent("Prueba");
+//			
+//			
+//			
+//		}
+//		
+//		//Creamos un segundo elemento, lo añadimos al final y le agregamos un hijo
+//		Element hijo2=new Element("Codigo");
+//		root.addContent(hijo2);
+//		Element sub2=new Element("Cod");
+//		hijo2.addContent(sub2.addContent("54321"));
+//		
+//		//A sub2 le agregamos un atributo de tipo int
+//		sub2.setAttribute("id", String.valueOf(50));
+//		
+//		
+//		XMLOutputter serializador=new XMLOutputter(Format.getPrettyFormat());
+//		try {
+//			serializador.output(root, System.out); //doc --> NOs imprime todo el documento junto a la version (root,System.out)
+//		} catch (IOException e) {
+//			e.printStackTrace();
+//		}
 
 		
 	}
